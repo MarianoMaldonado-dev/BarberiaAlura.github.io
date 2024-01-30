1318e29a0bbf90fa272209ee672a3793aa8751d3
Cyber War Games Productions <124847965+SuperMarianWorld@users.noreply.github.com>
2024-01-30 19:33:17 -0300

    Initial Commit

---
+++ Barbería Alura/Scripts/year_actualization.js
@@ -0,0 +1,7 @@
+copyright=new Date();
+                
+    update=copyright.getFullYear();
+                
+        document.write("© Copyright | " + update + " | " + "Barbería Alura");
+                
+//La funcionalidad de éste Script es simplemente actualizar el año automáticamente
